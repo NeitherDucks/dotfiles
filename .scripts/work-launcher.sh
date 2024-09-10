fe27b22edb0df9f03a399e0a1b8d73b5a1b95826
NeitherDucks <jolan92@gmail.com>
2024-09-10 14:14:48 -0400

    cleanup

--- .scripts/work-launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-zoom &
-chromium mail.google.com chat.google.com https://docs.google.com/spreadsheets/d/1SmKW4X4cC5eFHT3TNsOt8rzn4d1z3Bt930kKK7cXAxo/edit?gid=0#gid=0 &
-env WINEPREFIX="/home/jolan/.wine" wine C:\\ProgramData\\Microsoft\\Windows\\Start\ Menu\\Programs\\Anyware\\Anyware\ Client.lnk &
